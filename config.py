22530d6f00496517554dc6259c626f0d70493599
widhvans <110589162+widhvans@users.noreply.github.com>
2025-04-18 20:18:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
 # config.py
 BOT_TOKEN = "7953655254:AAGUxu-olhwtUhdjEigPFnQS8T3zoLNhnXs"  # Replace with your Telegram Bot Token from BotFather
-MAX_FILE_SIZE = 20 * 1024 * 1024  # 20 MB limit for free bots
-DOWNLOAD_DIR = "downloads"
-LOG_DIR = "logs"
+DOWNLOAD_DIR = "downloads"  # Directory for temporary file storage
